348d75bf4ef9287d94561f268406ff76ab95c42e
krgirard33 <ken.girard@gmail.com>
2023-04-28 15:54:00 -0500

    AddUser adds user to the db, then goes to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
-
-    const [users, setUsers]=useState([])
+    const [users, setUsers] = useState([]);
 
     useEffect(() => {
         loadUsers();
-    },[]);
+    }, []);
 
-    const loadUsers= async() => {
-        const result=await axios.get("http://localhost:8080/users");
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/users");
         setUsers(result.data);
-    }
+    };
 
     return (
-        <div className='container'>
-            <div className='py-4'>
+        <div className="container">
+            <div className="py-4">
                 <table className="table table-bordered border-primary">
                     <thead>
                         <tr>
@@ -28,28 +27,24 @@ export default function Home() {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            users.map((user,index)=>
-                                <tr>
-                                    <th scope="row" key={index}>
-                                        {index+1}
-                                        </th>
-                                    <td>{user.name}</td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <td>
-                                        <button className="btn btn-primary mx-2">View</button>
-                                        <button className="btn btn-outline-primary mx-2">Edit</button>
-                                        <button className="btn btn-danger mx-2">Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        }
-
-                        
+                        {users.map((user, index) => (
+                            <tr>
+                                <th scope="row" key={index}>
+                                    {index + 1}
+                                </th>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <button className="btn btn-primary mx-2">View</button>
+                                    <button className="btn btn-outline-primary mx-2">Edit</button>
+                                    <button className="btn btn-danger mx-2">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
         </div>
-    )
+    );
 }
