749aa9608f0f1742467cb8b718cda31cbc7b6770
krgirard33 <ken.girard@gmail.com>
2023-04-28 01:48:08 -0500

    Home shows data from db, axios installed

--- src/layout/navbar.js
+++ src/layout/navbar.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <a className="navbar-brand" href="/">Full Stack Application</a>
                     <button className="navbar-toggler" 
                         type="button" 
                         data-bs-toggle="collapse" 
@@ -16,6 +16,8 @@ export default function Navbar() {
                     >
                     <span className="navbar-toggler-icon"></span>
                     </button>
+
+                    <button className="btn btn-outline-light">Add User</button>
                     
                 </div>
             </nav>
---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+
+    const [users, setUsers]=useState([])
+
+    useEffect(() => {
+        loadUsers();
+    },[]);
+
+    const loadUsers= async() => {
+        const result=await axios.get("http://localhost:8080/users");
+        setUsers(result.data);
+    }
+
+    return (
+        <div className='container'>
+            <div className='py-4'>
+                <table className="table table-bordered border-primary">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user,index)=>
+                                <tr>
+                                    <th scope="row" key={index}>{index+1}</th>
+                                    <td>{user.name}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                </tr>
+                            )
+                        }
+
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
